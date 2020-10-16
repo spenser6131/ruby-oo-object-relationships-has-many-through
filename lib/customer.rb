5906448a325b7384c7d37214d06ad2e3d6dbd0cf
spenser6131 <brinkman.spenser@gmail.com>
2020-10-15 19:26:27 -0700

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,29 @@
 class Customer
+
+  attr_reader :name, :age
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    Meal.all.select {|meal| meal.customer == self}
+  end
+
+  def waiters
+    self.meals.collect {|meal| meal.waiter}
+  end
   
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,30 @@
 class Waiter
+  
+  attr_accessor :yrs_exp
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name, yrs_exp)
+    @name = name
+    @yrs_exp = yrs_exp
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    Meal.all.select {|meal| meal.waiter == self}
+  end
+
+  def best_tipper
+    meals.max_by {|meal| meal.tip}.customer
+  end
 
 end
